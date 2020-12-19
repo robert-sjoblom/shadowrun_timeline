0b9936e2e5603fa45d1c2f2cec9a0fb9b08f04ab
Robert Sjöblom <robert.sjoblom@gmail.com>
2020-12-19 11:28:08 +0100

    Fix date being raw and inaccurate perhaps

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,12 @@
 #
 # Table name: events
 #
-#  id          :uuid             not null, primary key
-#  date        :datetime
-#  description :text
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id            :uuid             not null, primary key
+#  date          :datetime
+#  date_accuracy :integer
+#  description   :text
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
 #
 class Event < ApplicationRecord
   validates :description, presence: true
