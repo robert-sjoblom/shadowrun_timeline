b28691c31039e710b8e35f3e6fd5465068a0c919
Robert Sjöblom <robert.sjoblom@gmail.com>
2020-12-20 09:35:34 +0100

    Use assign instead of instance var in spec

--- spec/views/events/edit.html.erb_spec.rb
+++ spec/views/events/edit.html.erb_spec.rb
@@ -3,14 +3,16 @@
 require "rails_helper"
 
 RSpec.describe "events/edit", type: :view do
+  let(:event) { create(:event) }
+
   before do
-    @event = create(:event)
+    assign(:event, event)
   end
 
   it "renders the edit event form" do
     render
 
-    assert_select "form[action=?][method=?]", event_path(@event), "post" do
+    assert_select "form[action=?][method=?]", event_path(event), "post" do
       assert_select "textarea[name=?]", "event[description]"
     end
   end
